262ca063f9b21d9d36f9fd9b8aa25ee324cf0090
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2024-10-29 22:47:17 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
+const fetch = require('node-fetch');
 
+async function verifyTurnstile(token) {
+    const response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            secret: '0x4AAAAAAAywgyZBo1sr9BT8_8gql-DobUQ',
+            response: token
+        })
+    });
+    return response.json();
+}
